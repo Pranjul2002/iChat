f5313c313b8f3317e2c0c642b700d48b8344c5df
Pranjul khankriyal <109353834+Pranjul2002@users.noreply.github.com>
2023-02-04 17:13:09 +0530

    multiClient Handeler

---
+++ client.java
@@ -0,0 +1,57 @@
+package chatApp.src;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.*;
+import java.util.Scanner;
+
+public class client{
+
+    Socket s;
+    Scanner in;
+    PrintStream out;
+
+    client() throws IOException{
+        s=new Socket("localhost",9999);
+
+        in=new Scanner(s.getInputStream());
+        out=new PrintStream(s.getOutputStream());
+
+        startReading();
+        startWriting();
+    }
+    
+    public void startReading(){
+        Runnable r1=new Runnable() {
+
+            @Override
+            public void run() {
+                while(true){
+                    String msgRecieve=in.nextLine();
+                    System.out.println(msgRecieve);
+                }
+            }
+        };
+        new Thread(r1).start();
+    }
+
+    public void startWriting(){
+        Runnable r2=new Runnable() {
+
+            @Override
+            public void run() {
+                while(true){
+                    Scanner scan=new Scanner(System.in);
+                    String sendMsg=scan.nextLine();
+                    out.println(sendMsg);
+                }
+            }
+        };
+        new Thread(r2).start();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        new client();
+    }
+}
---
+++ server.java
@@ -0,0 +1,30 @@
+package chatApp.src;
+
+import java.io.IOException;
+import java.net.*;
+
+
+public class server {
+
+    ServerSocket serverS;
+    int clientNum=0;
+
+    server() throws IOException{
+        serverS=new ServerSocket(9999);
+        System.out.println("Waiting...............");
+        while(true){
+            Socket client=serverS.accept();
+            clientNum++;
+            System.out.println("New client is Connected....");
+            clientHandler clientHandle=new clientHandler(client,clientNum);
+            Thread thread=new Thread(clientHandle);
+            thread.start();
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException{
+        new server();
+    }
+    
+}
